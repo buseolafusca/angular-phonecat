a8974486f58e908029489e18828517db7dc01742
buseolafusca <busseolafusca@gmail.com>
2015-12-07 14:31:20 +0100

    fetch phones from phones.json, use  service and test too

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,18 +1,13 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp',[]);
+var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope){
-	$scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http){
 
-	$scope.name = "World";
-});
+	$http.get('phones/phone.json').success(function(data){
+		$scope.phones = data.splice(0, 2);
+	});
 
+	$scope.orderProp = 'age';
+}]);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//i think unit tests are for the default page config on page load
+//while e2e are for testing pssible configs at runtime
+
 /* jasmine specs for controllers go here */
 
 // describe('controllers', function() {
@@ -11,14 +14,52 @@
 // });
 
 
-describe('PhoneListCtrl', function(){
+// describe('PhoneListCtrl', function(){
+// 	var scope, ctrl;
 
+// 	beforeEach(module('phonecatApp'));
+
+// 	beforeEach(inject(function($controller){
+// 		scope = {};
+// 		ctrl = $controller('PhoneListCtrl', { $scope:scope });
+// 	}));
+
+// 	it('should create "phones" model with 3 phones', function() {
+// 		expect(scope.phones.length).toBe(3);
+// 	});
+
+// 	it('should set the default value of orderProp model', function(){
+// 		expect(scope.orderProp).toBe('age');
+// 	});
+// });
+
+describe('PhoneCat controllers', function(){
+
+describe('PhoneListCtrl', function(){
+	var scope, ctrl, $httpBackend;
+
+	//Load app module definition before each test.
 	beforeEach(module('phonecatApp'));
 
-	it('should create "phones" model with 3 phones', inject(function($controller){
-		var scope = {},
-		ctrl = $controller('PhoneListCtrl', {$scope:scope});
+	beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+		$httpBackend = _$httpBackend_;
+		$httpBackend.expectGET('phones/phone.json').respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-		expect(scope.phones.length).toBe(3);
+		scope = $rootScope.$new();
+		ctrl = $controller('PhoneListCtrl', {$scope: scope});
+
 	}));
+
+	it('should create "phones" model with 2 phones fetched from xhr', function(){
+		expect(scope.phones).toBeUndefined();
+		$httpBackend.flush();
+
+		expect(scope.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+	});
+
+	it('should set the default value of orderProp model', function(){
+		expect(scope.orderProp).toBe('age');
+	});
+});
+
 });
